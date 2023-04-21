33bcc976192076f008963ffad7e0728ecaa3c945
Jimmy Vu <jimmyvu1357@gmail.com>
2023-04-21 14:31:37 -0700

    ...

--- src/trees/TreeException.java
+++ src/trees/TreeException.java
@@ -1,9 +1,11 @@
 package trees;
 
-public class TreeException extends ???
+public class TreeException extends FamilyTree
 {
 	public TreeException(String s)
 	{
 		// Pass s into a superclass ctor.
+		super(s);
+		
 	}
 }
